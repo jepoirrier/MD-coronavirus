35e3230bf02be22f106167e769c5e05013336e7c
Jean-Etienne Poirrier <jepoirrier@gmail.com>
2020-04-05 16:00:25 -0400

    Code for daily #, total # of tests and % positive

--- MD-coronavirus-cases.R
+++ MD-coronavirus-cases.R
@@ -1,9 +1,17 @@
 # Produce a simple graph of Coronavirus cases in Maryland State (USA) since the 1st cases detected
 # Source of data: https://coronavirus.maryland.gov/ (updated daily at ~ 10am - 12pm)
 # Rationale for doing this: no history of past cases and removal of some data (see below)
+# Explanations: https://jepoirrier.org
+# Data repository: https://github.com/jepoirrier/MD-coronavirus
 
+library(dplyr)
 library(ggplot2)
+library(ggpubr)
 library(tidyr)
+
+#source("fnMultiplot.R")
+
+# ********** Reading the number of cases file **********
 
 # ***** Trends in number of cases by totals over time *****
 
@@ -19,8 +27,8 @@ MDCasesNCols <- 6
 
 dat <- read.csv(MDCasesFile, sep = " ", colClasses = c(rep("numeric", MDCasesNCols)))
 
-lastDateCases <- as.Date(sprintf("%d",max(dat$Date)), "%y%m%d")
-lastMaxPositiveCases = max(dat$Positive)
+lastDateCases <- as.Date(sprintf("%d", max(dat$Date, na.rm = TRUE)), "%y%m%d")
+lastMaxPositiveCases = max(dat$Positive, na.rm = TRUE)
 
 cols2pivot <- colnames(dat) # headers of columns to pivot
 cols2pivot[2:6] <- c("Negative", "Positive", "Deaths", "Hospitalizations", "Released")
@@ -28,7 +36,7 @@ colnames(dat) <- cols2pivot
 cols2pivot <- cols2pivot[2:6] # we don't need "Date"
 
 dt <- pivot_longer(data = dat, cols = cols2pivot, names_to = "Total", values_to = "Tests") # don't drop NAs because Negative tests came back on 3/28
-dt$Date <- as.Date(sprintf("%d",dt$Date), "%y%m%d")
+dt$Date <- as.Date(sprintf("%d", dt$Date), "%y%m%d")
 
 p <- ggplot(dt, aes(x = Date, y = Tests, group = Total)) +
   # log scale and 33% daily as in FT graph
@@ -41,15 +49,100 @@ p <- ggplot(dt, aes(x = Date, y = Tests, group = Total)) +
        y = "Tests counts (log scale!)",
        caption = paste("Data from https://coronavirus.maryland.gov/ ; explanations at https://jepoirrier.org ; last update:", format(Sys.Date(), "%b %d, %Y"))) +
   # manually place the arrow and label highlighting the last data
-  annotate("segment", x = as.Date(lastDateCases) -0.5 , y = lastMaxPositiveCases + 1000,
-           xend = as.Date(lastDateCases), yend = lastMaxPositiveCases + 100,
+  annotate("segment", x = as.Date(lastDateCases) - 3.5, y = lastMaxPositiveCases,
+           xend = as.Date(lastDateCases) - 2, yend = lastMaxPositiveCases,
            size = 0.5, arrow = arrow(length = unit(.2, "cm"))) +
-  annotate("text", label = paste("Last number of\ncases:", lastMaxPositiveCases),
-           x = as.Date(lastDateCases) - 1, y = lastMaxPositiveCases + 2000,
+  annotate("text", label = paste("Last number of\ncases:", format(lastMaxPositiveCases, scientific = FALSE, big.mark = ",")),
+           x = as.Date(lastDateCases) - 6, y = lastMaxPositiveCases + 1000,
            size = 3, fontface = "italic")
 
 p # optional
 ggsave("MD-coronavirus-cases.png", plot = p, device = "png", width = 3840/300, height = 2160/300, units = "in")
+
+# ***** Trends in number of DAILY cases over time *****
+
+# compute delta per day
+dat <- dat %>%
+  mutate(deltaNegative = Negative - lag(Negative, default = 0))
+dat <- dat %>%
+  mutate(deltaPositive = Positive - lag(Positive, default = 0))
+dat <- dat %>%
+  mutate(deltaDeaths = Deaths - lag(Deaths, default = 0))
+dat <- dat %>%
+  mutate(deltaHospitalizations = Hospitalizations - lag(Hospitalizations, default = 0))
+dat <- dat %>%
+  mutate(deltaReleased = Released - lag(Released, default = 0))
+
+cols2pivot <- colnames(dat) # headers of columns to pivot
+cols2pivot[2:6] <- c("TotalNegative", "TotalPositive", "TotalDeaths", "TotalHospitalizations", "TotalReleased")
+cols2pivot[7:11] <- c("Negative", "Positive", "Deaths", "Hospitalizations", "Released")
+colnames(dat) <- cols2pivot
+cols2pivot <- cols2pivot[7:11] # we don't need "Date"
+
+dt <- pivot_longer(data = dat, cols = cols2pivot, names_to = "Delta", values_to = "DailyVariation") # don't drop NAs because Negative tests came back on 3/28
+dt$Date <- as.Date(sprintf("%d",dt$Date), "%y%m%d")
+
+p <- ggplot(dt, aes(x = Date, y = DailyVariation, group = Delta)) +
+  # log scale and 33% daily as in FT graph
+  #scale_y_log10() + annotation_logticks() +
+  #geom_abline(intercept = 1, slope = 33) +
+  geom_line(aes(color = Delta)) +
+  geom_point(aes(color = Delta, shape = Delta)) +
+  labs(title = "Daily variation of Coronavirus cases in Maryland, USA (2020)",
+       x = "Date",
+       y = "Daily variation (# of cases)",
+       caption = paste("Data from https://coronavirus.maryland.gov/ ; explanations at https://jepoirrier.org ; last update:", format(Sys.Date(), "%b %d, %Y")))
+
+p # optional
+ggsave("MD-coronavirus-cases-daily.png", plot = p, device = "png", width = 3840/300, height = 2160/300, units = "in")
+
+# ***** Displaying total number of tests made & the positive ratio *****
+
+datTT <- dat[1:3]
+datTT$TotalTests <- datTT$TotalNegative + datTT$TotalPositive
+datTT$PositivePC <- datTT$TotalPositive / datTT$TotalTests * 100
+
+lastMaxTotalCases = max(datTT$TotalTests, na.rm = TRUE)
+lastPositivePC = if (is.na(datTT$PositivePC[nrow(datTT)])) (datTT$PositivePC[nrow(datTT) - 1]) else (datTT$PositivePC[nrow(datTT)]) # needed because sometimes last line is NA
+
+datTT$Date <- as.Date(sprintf("%d", datTT$Date), "%y%m%d")
+
+p <- ggplot(datTT, aes(x = Date, y = TotalTests)) +
+  geom_line() +
+  geom_point() +
+  labs(title = "Cumulative number of Coronavirus tests received in Maryland, USA (2020)",
+       x = " ",
+       y = "Cumulative number of tests") +
+  # manually place the arrow and label highlighting the last data
+  annotate("segment", x = as.Date(lastDateCases) - 3.5, y = lastMaxTotalCases,
+           xend = as.Date(lastDateCases) - 2, yend = lastMaxTotalCases,
+           size = 0.5, arrow = arrow(length = unit(.2, "cm"))) +
+  annotate("text", label = paste("Last total number\nof tests:", format(lastMaxTotalCases, scientific = FALSE, big.mark = ",")),
+           x = as.Date(lastDateCases) - 6, y = lastMaxTotalCases + 1000,
+           size = 3, fontface = "italic")
+
+q <- ggplot(datTT, aes(x = Date, y = PositivePC)) +
+  geom_line() +
+  geom_point() +
+  labs(title = "Proportion of positive Coronavirus tests in Maryland, USA (2020)",
+       x = "Date",
+       y = "Percentage of positive tests (%)",
+       caption = paste("Data from https://coronavirus.maryland.gov/ ; explanations at https://jepoirrier.org ; last update:", format(Sys.Date(), "%b %d, %Y"))) +
+  # manually place the arrow and label highlighting the last data
+  annotate("segment", x = as.Date(lastDateCases) - 3.5, y = lastPositivePC,
+           xend = as.Date(lastDateCases) - 2, yend = lastPositivePC,
+           size = 0.5, arrow = arrow(length = unit(.2, "cm"))) +
+  annotate("text", label = paste("Last percentage of\npositive tests:", sprintf("%.2f%%", lastPositivePC)),
+           x = as.Date(lastDateCases) - 6.5, y = lastPositivePC,
+           size = 3, fontface = "italic")
+
+#multiplot(p, q, cols = 1)
+r <- ggarrange(p, q, heights = c(1, 1), 
+          ncol = 1, nrow = 2, align = "v")
+
+ggsave("MD-coronavirus-total-tests.png", plot = r, device = "png", width = 3840/300, height = 2160/300, units = "in")
+
+# ********** Reading the number of cases in COUNTIES file **********
 
 # ***** Trends in number of cases by COUNTY over time *****
 
@@ -80,6 +173,8 @@ p <- ggplot(dt, aes(x = Date, y = Tests, group = County)) +
        caption = paste("Data from https://coronavirus.maryland.gov/ ; explanations at https://jepoirrier.org ; last update:", format(Sys.Date(), "%b %d, %Y")))
 p # optional
 ggsave("MD-coronavirus-counties.png", plot = p, device = "png", width = 3840/300, height = 2160/300, units = "in")
+
+# ********** Reading the number of cases BY AGE file **********
 
 # ***** Trends in number of cases by age group over time *****
 
