23035499dff9572976025df34bb86266c740b6c2
Jean-Etienne Poirrier <jepoirrier@gmail.com>
2020-05-06 13:55:54 -0400

    Added simple script to wrangle zip data

---
+++ parse-zip-v2.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+For files where I pasted the data from the MDH dashboard
+
+TODO: define the date variable!
+
+Created on Tue Apr 14 21:36:28 2020
+
+@author: jepoirrier
+"""
+
+# TODO CHANGE -- 1 --- DATE
+
+
+import os
+
+currentDate = '200506'
+
+os.chdir('/Users/jepoirrier/Documents/software/MD-coronavirus-cases')
+
+inputFileName = 'MD-corona-ZIPpasted' + currentDate + '.txt'
+outputFileName = 'MD-corona-ZIP.txt'
+
+inputFile = open(inputFileName, mode = "r")
+outputFile = open(outputFileName, mode = "a")
+
+for line in inputFile:
+    elems = line.split()
+    if len(elems) == 4:
+        outputFile.write(currentDate + " " + elems[0] + " " + elems[2] + "\n")
+
+outputFile.close()
+inputFile.close()
