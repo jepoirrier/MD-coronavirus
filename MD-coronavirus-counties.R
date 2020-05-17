6d07345cb4e94d841a857c392249beb1ae6b8bc2
Jean-Etienne Poirrier <jepoirrier@gmail.com>
2020-05-16 22:42:16 -0400

    Code & figure for county death rate

--- MD-coronavirus-counties.R
+++ MD-coronavirus-counties.R
@@ -20,6 +20,8 @@ plotHeightLong <- 10 # for multiple graphs: 9 (= 3 times 3) + 1
 iniFile <- "data-other-sources/pointData.ini"
 pointData <- read.ini(iniFile)
 
+# *********** RELATIVE CASES **********
+
 # Read the data from the counties
 MDCountiesFile <- 'MD-coronavirus-counties.txt'
 MDCountiesNCols <- 26
@@ -89,7 +91,8 @@ q <- ggplot(dt, aes(x = Date, y = Tests, group = County)) +
            x = as.Date(as.Date(sprintf("%d", max(datX$Date)), "%y%m%d")) - 5, y = cphtMD + 20,
            size = 3, fontface = "italic") +
   theme_linedraw() +
-  labs(x = "Date",
+  labs(title = "Evolution of COVID-19 confirmed cases in Maryland counties, USA (2020)",
+       x = "Date",
        y = "Cumulative cases / 100,000 pop",
        caption = paste("DnA = Data not Available ; US data: OurWorldInData.org ; MD average: CDC (both", dateOtherDataUpdate, ")\nCOVID-19 data from https://coronavirus.maryland.gov/ ; explanations at https://jepoirrier.org/mdcovid19/ ; last update:", format(Sys.Date(), "%b %d, %Y")))
 
@@ -98,3 +101,64 @@ r <- ggarrange(p, q, heights = c(1, 1),
 
 ggsave("figures/MD-COVID19-counties-casesPHT.png", plot = r, device = "png", width = plotWidth, height = plotHeightLong, units = "in")
 
+# *********** RELATIVE DEATHS **********
+
+# Read the data from the deaths in counties
+MDCountiesDeathsFile <- 'MD-coronavirus-counties-deaths.txt'
+MDCountiesDeathsNCols <- 25
+
+datCountyDeaths <- read.csv(MDCountiesDeathsFile, sep = " ", colClasses = c(rep("numeric", MDCountiesDeathsNCols)))
+lastDateCountyDeaths <- as.Date(sprintf("%d",max(datCountyDeaths$Date)), "%y%m%d")
+lastMaxCountyDeaths = max(datCountyDeaths$PrinceGeorges) # TODO manually find the county with max deaths - TODO change this behavior
+
+datCountyDeaths$DnA <- NULL
+
+cols2pivot <- colnames(datCountyDeaths)
+cols2pivot <- cols2pivot[2:length(cols2pivot)] # we don't need "Date"
+
+dt <- pivot_longer(data = datCountyDeaths, cols = cols2pivot, names_to = "County", values_to = "Deaths", values_drop_na = TRUE)
+dt$Date <- as.Date(sprintf("%d",dt$Date), "%y%m%d")
+
+p <- ggplot(dt, aes(x = Date, y = Deaths, group = County)) +
+  geom_line(aes(color = County), lwd = 1) +
+  geom_point(aes(color = County, shape = County)) +
+  theme_linedraw() +
+  #gghighlight(County == "Charles") +
+  labs(title = "Evolution of COVID-19-confirmed deaths in Maryland counties, USA (2020)",
+       x = "Date",
+       y = "Cumulative number of deaths")
+
+
+
+matC <- as.matrix(datCountyDeaths) # transform to matrix for processing
+datCountyPop$DnA <- NULL
+matP <- as.matrix(datCountyPop)
+
+matX <- sweep(matC, 2, matP, '/') # simple processing
+matX <- matX * 100000
+matX[,1] <- matX[,1] / 100000 # well, shouldn't have multiplied for Date ...
+
+datX <- as.data.frame(matX) # get back to dataframe
+
+datX$DnA <- NULL # removing DnA as it messes with the graph
+
+cols2pivot <- colnames(datX)
+cols2pivot <- cols2pivot[2:length(cols2pivot)] # we don't need "Date" nor DnA
+
+dt <- pivot_longer(data = datX, cols = cols2pivot, names_to = "County", values_to = "Deaths", values_drop_na = TRUE)
+dt$Date <- as.Date(sprintf("%d",dt$Date), "%y%m%d")
+
+q <- ggplot(dt, aes(x = Date, y = Deaths, group = County)) +
+  geom_line(aes(color = County), lwd = 1) +
+  geom_point(aes(color = County, shape = County)) +
+  theme_linedraw() +
+  #gghighlight(County == "Charles") +
+  labs(title = "Evolution of COVID-19-specific death rate, USA (2020)",
+       x = "Date",
+       y = "Cumulative number of deaths / 100,000 pop",
+       caption = paste("Data from https://coronavirus.maryland.gov/ ; explanations at https://jepoirrier.org/mdcovid19/ ; last update:", format(Sys.Date(), "%b %d, %Y")))
+
+r <- ggarrange(p, q, heights = c(1, 1), 
+               ncol = 1, nrow = 2, align = "v")
+
+ggsave("figures/MD-COVID19-counties-deathsPHT.png", plot = r, device = "png", width = plotWidth, height = plotHeight, units = "in")
