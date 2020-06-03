cad57b4d6c7ce6375fe3d720cd90debd15f98739
Jean-Etienne Poirrier <jepoirrier@gmail.com>
2020-06-03 16:58:55 -0400

    Added trend on death by date

--- src/deaths-confirmed.R
+++ src/deaths-confirmed.R
@@ -14,11 +14,13 @@
 plotWidth <- 12
 plotHeight <- 7 # for single graph: 6 (= 2 times 3) + 1
 plotHeightLong <- 10 # for multiple graphs: 9 (= 3 times 3) + 1
+Nbreaks <- 10 # default number of breaks for trend decomposition
 
 library(dplyr)
 library(gghighlight)
 library(ggplot2)
-library(ggseas)
+library(ggseas) # for decompose
+library(scales) # for x-axis ticks
 library(tidyr)
 
 # CUMULATIVE death confirmed
@@ -53,6 +55,9 @@ deltaF <- function(x, na.rm = FALSE) (x - lag(x, default = 0))
 dtDC <- datDC
 dtDC <- mutate_all(dtDC[2:length(dtDC)], ~ deltaF(.))
 dtDC <- cbind(dtDC, Date = datDC$Date) # this is risky (desync?) but adds Date as the last column
+
+# Get start / end dates to define Nbreaks
+Nbreaks <- floor((as.integer(max(dtDC$Date)) - as.integer(min(dtDC$Date))) / 5)
 
 # DAILY CONFIRMED DEATHS
 
@@ -103,7 +108,7 @@ p <- ggplot(datDC, aes(x = Date, y = Confirmed)) +
   annotate("text", label = paste("Latest:", datDC$Confirmed[datDC$Date == max(datDC$Date)], "\nconfirmed"),
            x = max(datDC$Date) - 2, y = datDC$Confirmed[datDC$Date == max(datDC$Date)] - 400,
            size = 4, fontface = "italic")
-p
+#p
 
 
 # CHART ALL DAILY DEATHs over time
@@ -123,11 +128,11 @@ q <- ggplot(dtDCDlong, aes(x = Date, y = Count, group = Deaths)) +
   annotate("text", label = paste("Maximum daily confirmed death:", max(dtDC$Confirmed)),
            x = min(dtDC$Date[dtDC$Confirmed == max(dtDC$Confirmed)]) + 5, y = max(dtDC$Confirmed) + 10,
            size = 4, fontface = "italic")
-q
+#q
 
 r <- ggarrange(p, q, heights = c(1, 1), 
                ncol = 1, nrow = 2, align = "v")
-r
+#r
 ggsave("../figures/deaths-confirmed.png", plot = r, device = "png", width = plotWidth, height = plotHeightLong, units = "in")
 
 
@@ -143,12 +148,26 @@ ggsave("../figures/deaths-confirmed.png", plot = r, device = "png", width = plot
 s <- ggsdc(dtDC, aes(x = Date, y = Confirmed), frequency = 7, method = 'decompose') +
   geom_line() +
   theme_linedraw() +
+  scale_x_date(breaks = pretty_breaks(Nbreaks)) +
   #theme(legend.position = "none") +
   labs(title = "Evolution of daily Coronavirus confirmed deaths in Maryland, USA (2020)",
-       x = "Date",
+       x = "Date of reporting",
        y = "Daily confirmed deaths",
        caption = paste("Explanations at https://jepoirrier.org/mdcovid19/ - data from https://coronavirus.maryland.gov/ - last update:", format(Sys.Date(), "%b %d, %Y")))
+#s
+ggsave("../figures/deaths-confirmedTrend.png", plot = s, device = "png", width = plotWidth, height = plotHeight, units = "in")
 
-ggsave("../figures/deaths-confirmedTrend.png", plot = r, device = "png", width = plotWidth, height = plotHeight, units = "in")
+
+t <- ggsdc(datDCD, aes(x = Date, y = ByDate), frequency = 7, method = 'decompose') +
+  geom_line() +
+  theme_linedraw() +
+  scale_x_date(breaks = pretty_breaks(Nbreaks)) +
+  #theme(legend.position = "none") +
+  labs(title = "Evolution of Coronavirus deaths (by date of death) in Maryland, USA (2020)",
+       x = "Date of death",
+       y = "Daily confirmed deaths",
+       caption = paste("Explanations at https://jepoirrier.org/mdcovid19/ - data from https://coronavirus.maryland.gov/ - last update:", format(Sys.Date(), "%b %d, %Y")))
+#t
+ggsave("../figures/deaths-confirmedTrendD.png", plot = t, device = "png", width = plotWidth, height = plotHeight, units = "in")
 
 
