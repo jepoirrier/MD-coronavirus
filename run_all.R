3058dfa1a2bfba998249a51c483a8653d5391b66
Jean-Etienne Poirrier <jepoirrier@gmail.com>
2020-07-23 19:29:51 -0400

    (m) added parenthesis in comment

--- run_all.R
+++ run_all.R
@@ -8,7 +8,7 @@ source("MD-coronavirus-counties.R")
 #source("MD-coronavirus-races.R") # now replaced below, can be deleted when sure API stable
 source("MD-coronavirus-Stage1.R")
 
-# NEW VERSION of code (API data
+# NEW VERSION of code (API data)
 # Note: pay attention to last data in data sets. Sometimes, re-downloading give the latest data
 setwd("src/")
 source("deaths-confirmed.R")
