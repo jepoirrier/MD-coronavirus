3a36d1715108702703eab4903f154e29a26c3be2
Jean-Etienne Poirrier <jepoirrier@gmail.com>
2020-08-09 15:07:49 +0200

    Cleanup duplicate (old) code
    County analysis still to be done!

--- src/run_all.R
+++ src/run_all.R
@@ -1,17 +1,9 @@
 # run_all() just source all other code
-# TODO move it to src too
-# but for the moment, it starts in ..
-
-# ORIGINAL code (manual data)
-source("MD-coronavirus-cases.R")
-source("MD-coronavirus-counties.R")
-#source("MD-coronavirus-races.R") # now replaced below, can be deleted when sure API stable
-source("MD-coronavirus-Stage1.R")
 
 # NEW VERSION of code (API data)
 # Note: pay attention to last data in data sets. Sometimes, re-downloading give the latest data
-setwd("src/")
 source("cases-analysis.R")
+source("hospit-analysis.R")
 source("deaths-confirmed.R")
 source("age-analysis.R")
 source("gender-analysis.R") # sometimes needs to be re-run a 2nd time to get today's data
